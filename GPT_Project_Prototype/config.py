0bfbf196955aeea2a99d77c9396914db4e23742e
gokeay <ozcan301gokay@gmail.com>
2023-08-15 18:48:02 +0300

    Updated

--- GPT_Project_Prototype/config.py
+++ GPT_Project_Prototype/config.py
@@ -1,19 +1,24 @@
 def clear(args):
     # control if function is called or not
-    print("===> clear(color) function is called correctly." + "\n")
+    color = list(args.values())[0]
+    direction = list(args.values())[1]
+    print("===> clear(color) function is called correctly.", color, direction + "\n")
 
-def pick(args):
+def pickUp(args):
     # control if function is called or not
-    print("===> pick(color) function is called correctly with argument ...." + "\n") # f-string
+    color = list(args.values())[0]
+    print("===> pickUp(color) function is called correctly with argument ", color + "\n") # f-string
 
 def place(args):
-    x,y,z = args.get("position")
+    x = args.get("x")
+    y = args.get("y")
+    z = args.get("z")
     # control if function is called or not
-    print("===> place(position_x, position_y, position_z) function is called correctly." + "\n")
+    print("===> place(position_x, position_y, position_z) function is called correctly.",str(x), str(y), str(z) + "\n")
 
-def put_on(args):
+def putOn(args):
     # control if function is called or not
-    print("===> put_on(color_1, color_2) function is called correctly." + "\n")
+    print("===> putOn(color_1, color_2) function is called correctly." + "\n")
 
 def push(args):
     # control if function is called or not
@@ -21,6 +26,10 @@ def push(args):
 
 def position(args):
   return 1,5,3
+
+def give(args):
+    # control if function is called or not
+    print("===> give() function is called correctly." + "\n")
 
 
 # taken/picked-up
@@ -33,30 +42,30 @@ functions = [
               "type": "object",
               "properties": {
 
-                  "clear": {
-                      "name": "clear",
-                      "description": "Before take the any object check if there are any other objects around the block of the desired color that the robot arm will pick up, which could obstruct it.",
-                      "parameters": {
-                          "type": "object",
-                          "properties": {
-                              "color": {
-                                  "type": "string",
-                                  "description": "The color of the object whose surroundings will be checked, e.g. 'blue', 'red', 'green' ",
-                              },
-                          },
-                          "required": ["color"],
-                      },
-                  },
+                #   "clear": {
+                #       "name": "clear",
+                #       "description": "Before take the any object check if there are any other objects around the block of the desired color.",
+                #       "parameters": {
+                #           "type": "object",
+                #           "properties": {
+                #               "color": {
+                #                   "type": "string",
+                #                   "description": "The color of the object whose surroundings will be checked, e.g. 'blue', 'red', 'green' ",
+                #               },
+                #           },
+                #           "required": ["color"],
+                #       },
+                #   },
 
-                  "pick": {
-                      "name": "pick",
-                      "description": "Pick up the desired color object with the robot arm.",
+                  "pickUp": {
+                      "name": "pickUp",
+                      "description": "Pick up the desired color object.",
                       "parameters": {
                           "type": "object",
                           "properties": {
                               "color": {
                                   "type": "string",
-                                  "description": "The color of the object to be picked up by the robot arm, e.g. 'blue', 'red', 'green' ",
+                                  "description": "The color of the object to be picked up. It could be only one of these: 'blue', 'red', 'green' ",
                               },
                           },
                           "required": ["color"],
@@ -65,86 +74,99 @@ functions = [
                   
                   "place": {
                       "name": "place",
-                      "description": "Move the desired object to the specified x y z position using the robot arm.",
+                      "description": "Move the desired color object to the specified x y z position.",
                       "parameters": {
                           "type": "object",
                           "properties": {
                               "position_x": {
-                                  "type": "integer",
-                                  "description": "The x value of the target position, e.g. 3, 1, 5 ",
+                                  "type": "float",
+                                  "description": "The x value of the target position in range of (0.6, 0.8), e.g. 0.648.",
                               },
                               "position_y": {
-                                  "type": "integer",
-                                  "description": "The y value of the target position, e.g. 3, 1 ,5 ",
+                                  "type": "float",
+                                  "description": "The y value of the target position in range of (-0.1, 0.5), e.g. 0.2.",
                               },
                               "position_z": {
-                                  "type": "integer",
-                                  "description": "The z value of the target position, e.g. 3, 1, 5 ",
+                                  "type": "float",
+                                  "description": "The z value of the target position in range of (-12, -0.1), e.g. -0.1.",
                               },
                           },
                           "required": ["position_x", "position_y", "position_z"],
                       },
                   },
 
-                  "put_on": {
-                      "name": "put_on",
-                      "description": "Place an object of the desired color onto an object of the specified color using the robot arm.",
+                  "putOn": {
+                      "name": "putOn",
+                      "description": "Place the picked object onto an object with specified color.",
                       "parameters": {
                           "type": "object",
                           "properties": {
-                              "picked_color": {
+                            #   "picked_color": {
+                            #       "type": "string",
+                            #       "description": "The color of the object to be picked up, e.g. 'blue', 'red', 'green' ",
+                            #   },
+                              "specified_color": {
                                   "type": "string",
-                                  "description": "The color of the object to be picked up by the robot arm, e.g. 'blue', 'red', 'green' ",
-                              },
-                              "target_color": {
-                                  "type": "string",
-                                  "description": "The color of target object which taken block will be placed by robot arm, e.g. 'blue', 'red', 'green' ",
+                                  "description": "The color of target object which taken block will be placed. It could be only one of these: 'blue', 'red', 'green' ",
                               },
                           },
-                          "required": ["picked_color", "target_color"],
+                          "required": ["specified_color"],
                       },
                   },
 
                   "push": {
                       "name": "push",
-                      "description": "Topple the structure made of objects by pushing the block of the desired color and in the desired direction by robot arm.",
+                      "description": "Topple the structure made of objects by pushing the block of the desired color and in the desired direction.",
                       "parameters": {
                           "type": "object",
                           "properties": {
                               "color": {
                                   "type": "string",
-                                  "description": "The color of the object to be pushed with the robot arm, e.g. 'blue', 'red', 'green' ",
+                                  "description": "The color of the object to be pushed, e.g. 'blue', 'red', 'green' ",
+                              },
+                              "direction": {
+                                  "type": "string",
+                                  "description": " The direction of the push. It could be only one of these: +x, -x, +y, -y",
+                              },
+                              "distance": {
+                                  "type": "float",
+                                  "description": "The amount of the displacement in the given direction in centimetre.",
                               },
                           },
                           "required": ["color"],
                       },
+                  },
+
+                  "give": {
+                    "name": "give",
+                    "description": "Take the desired color object then move it to the someone.",
+                    "parameters": "null"
                   }
-              }, "required": ["clear", "pick", "place", "put_on", "push"],
+
+              }, "required": ["clear", "pickUp", "place", "putOn", "push", "give"],
           }
       }
 ]
 
 available_functions = {
-        "clear": clear,
-        "pick": pick,
+        "pickUp": pickUp,
         "place": place,
-        "put_on": put_on,
+        "putOn": putOn,
         "push": push,
+        "give": give,
         # add you other avaliable functions
 }
 
-explanations_of_predicates = "obj(x) represent, x is an object. color(x,'red') represent, x object is red. near(x,y) represent, x object is near y object. on(x,y) represent, x object is on the top side of y object. position(x, 0.1, 0.2, 0) represent, x object is in the position x=0.1 y=0.2 z=0."
-
+explanations_of_predicates = "obj(x) represent, x is an object. color(x,'red') represent, x object is red."
+# position(x, 0.1, 0.2, 0) represent, x object is in the position x=0.1 y=0.2 z=0."
 predicates = {
-    "obj(x)",
-    "obj(y)",
-    "obj(z)",
-    "color(x,'red')",
-    "color(y,'blue')",
-    "color(z,'green')",
-    "near(y,z)",
-    "on(y,x)",
-    "positions(x, 0.1, 0.2, 0))",
-    "positions(y, 0.1, 0.5, 0))",
-    "positions(z, 0.4, 0.5, 0))"
+    "obj(1)",
+    "obj(2)",
+    "obj(3)",
+    "color(1,'red')",
+    "color(2,'blue')",
+    "color(3,'green')"
+    # "positions(1, 0.75, 0.2, -0.1))",
+    # "positions(2, 0.78, 0.1, -0.1))",
+    # "positions(3, 0.73, 0.3, -0.1))"
 }
--- GPT_Project_Prototype/main.py
+++ GPT_Project_Prototype/main.py
@@ -14,37 +14,55 @@ openai.api_key = apiKeys.OPENAI_API_KEY
 # Variable to clear the order in which the function is called
 seq_of_func = 1
 
+
 # main function which is called by user
 def run_conversation(user_request):
 
   combined_content = explanations_of_predicates + "\n" + "\n".join(predicates) + "\n" + user_request
-  messages = [{"role": "system", "content": "You are an assistant that always replies with multiple function calls in straight JSON format ready to parsed. Respond to user request based on predicates."},
-              {"role": "user", "content": combined_content}]
+  messages = [{"role": "system", "content": "You are the assistant that always reply with multiple function calls with only arguments of functions (If a function doesn't take input, definitely don't return an argument.) in string format instead of dictionary. For example; instead of {'color': 'blue', 'direction': '+x'} return blue +x. Only reply with given function parameters."},
+              #  + explanations_of_predicates + "Use predicaters to vizualize the environment to take action."},
+              #  You are an assistant that always replies with multiple function calls in straight JSON format ready to parsed. 
+              {"role": "user", "content": user_request}]
   
-  response = openai.ChatCompletion.create( 
+  response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo-0613",
       messages=messages,
       functions=functions,
       function_call="auto",
+      temperature=0.0,
   )
   print("\n" + "Respone: " + str(response) + "\n" + "\n" + "\n")
   
+  baxter_message = "pauseInterpreter;"
+  
   response_message = response["choices"][0]["message"]
-  print("Respone_message: " + str(response_message) + "\n" + "\n" + "\n")
+  # print("Respone_message: "  + str(response_message) + "\n" + "\n" + "\n")
+  print("Respone_message: " + str(response_message) + "\n\n\n")
+  
 
   if response_message.get("function_call"):
     function_args_str = response_message["function_call"]["arguments"]
     function_args = json.loads(function_args_str)
 
     for function_name, args in function_args.items():
-      function_to_call = avaliable_functions[function_name]
+      # function_to_call = avaliable_functions[function_name]
+      baxter_message += function_name
+      baxter_message += " "
+      baxter_message += str(args)
+      baxter_message += ";"
       print("function_name: " + function_name)
       print("args: ", args)
-      function_to_call(args)
 
-user_request_1 = "Could you pick the blue block and then put onto red block?"
-user_request_2 = "could you take down the tower of blocks with the blue block in left direction ?"
-user_request_3 = "Take the blue block."
 
-run_conversation(user_request = user_request_3)
+      # function_to_call(args)
+  baxter_message +="homePosition;pauseInterpreter"
+  print("baxter_message: " + baxter_message)
+  
+user_request_1 = "There are red and blue boxes on the table. Pick the blue box and then put onto red box."
+user_request_2 = "There are red and blue boxes on the table. Take down the tower of blocks with the blue block in +x direction."
+user_request_3 = "There are red and blue boxes on the table. Take the blue block."
+user_request_4 = "Move the blue block to the x=4, y=3, z=0."
+user_request_5 = "There are red and blue boxes on the table. Give the blue box to me."
+
+run_conversation(user_request = user_request_5)
 # print("\n\nFinal GPT output:\n" + a)
